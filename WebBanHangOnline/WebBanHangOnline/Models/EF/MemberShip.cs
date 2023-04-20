bb64588b493e9eb8f596dc4280c2f0a45eaf81d3
Phat <nguyentienphat050702@gmail.com>
2023-04-20 13:32:03 +0700

    v2

---
+++ WebBanHangOnline/WebBanHangOnline/Models/EF/MemberShip.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebBanHangOnline.Models.EF
+{
+    [Table("tb_Membership")]
+    public class MemberShip : CommonAbstract
+    {
+        [Key]
+        [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(150)]
+        public string Title { get; set; }
+        [StringLength(500)]
+        public decimal FromMoney { get; set; }
+        [StringLength(500)]
+        public decimal ToMoney { get; set; }
+        [StringLength(500)]
+        public float Discount { get; set; }
+    }
+}
